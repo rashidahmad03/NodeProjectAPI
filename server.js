02812d7c55515db814b0d332783fa5cdf6f35bfd
Rashid Ahmad <rashidahmad2135@gmail.com>
2019-09-06 00:55:34 +0530

    make it better

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const app = express();
  
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>'
-app.use(express.static(__dirname + '/dist/AngularResume'));
+app.use(express.static(__dirname + '/dist/RashidAhmad'));
  
 app.get('*', function(req,res) {
   // Replace the '/dist/<to_your_project_name>/index.html'
-  res.sendFile(path.join(__dirname + '/dist/AngularResume/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/RashidAhmad/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
